db4241462f88fabf2fdc64e03c625c72171bc522
Adam Loftin <aloftin@rxbenefits.com>
2021-12-03 21:23:54 -0600

    Completed day 2 part 1

---
+++ 2/day2_test.go
@@ -0,0 +1,8 @@
+package main
+
+import "testing"
+
+func TestPartOne(t *testing.T) {
+	lines := getLinesFromFile()
+	runPartOne(lines)
+}
---
+++ 2/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	lines := getLinesFromFile()
+	fmt.Printf("Part one answer: %v\n", runPartOne(lines))
+}
+
+func runPartOne(lines []string) int {
+	positions := make(map[string]int)
+	for _, line := range lines {
+		// split line by space char
+		// first half is the string key ("forward", "up", "down")
+		// second half is the int value
+		splitLine := strings.Split(line, " ")
+		positions[splitLine[0]] += toInt(splitLine[1])
+	}
+
+	answer := positions["forward"] * (positions["down"] - positions["up"])
+	return answer
+}
+
+func getLinesFromFile() []string {
+	file, err := os.Open("./input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	lines := make([]string, 0)
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		lines = append(lines, line)
+	}
+
+	return lines
+}
+
+func toInt(s string) int {
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return i
+}
