5236bcb47605a9295b09b4dfa9f234a1ab2a9fb5
Adam Loftin <aloftin@rxbenefits.com>
2021-12-03 21:37:12 -0600

    Completed day 2 part 2

--- 2/day2_test.go
+++ 2/day2_test.go
@@ -6,3 +6,8 @@ func TestPartOne(t *testing.T) {
 	lines := getLinesFromFile()
 	runPartOne(lines)
 }
+
+func TestPartTwo(t *testing.T) {
+	lines := getLinesFromFile()
+	runPartTwo(lines)
+}
--- 2/main.go
+++ 2/main.go
@@ -12,6 +12,32 @@ import (
 func main() {
 	lines := getLinesFromFile()
 	fmt.Printf("Part one answer: %v\n", runPartOne(lines))
+	fmt.Printf("Part two answer: %v\n", runPartTwo(lines))
+}
+
+func runPartTwo(lines []string) int {
+	var forward, up, down, aim int
+	for _, line := range lines {
+		// split line by space char
+		// first half is the string key ("forward", "up", "down")
+		// second half is the int value
+		splitLine := strings.Split(line, " ")
+
+		v := toInt(splitLine[1])
+		if splitLine[0] == "forward" {
+			forward += v
+			down += aim * v
+		}
+		if splitLine[0] == "up" {
+			aim -= v
+		}
+		if splitLine[0] == "down" {
+			aim += v
+		}
+	}
+
+	answer := forward * (down - up)
+	return answer
 }
 
 func runPartOne(lines []string) int {
